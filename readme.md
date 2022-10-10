35a923064aee4918dbf904c427d2e10e5b47d03e
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-10-10 19:15:21 +0530

    init

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Youtube Analysis
+
+## Installation
+
+`py -m venv env`
+
+`pip install -r requirements.txt`
+
+## After Installation
+
+- Open terminal. U should see a (env) symbol
+- Run the code by `py youtube.py`
+- A response.json will be generated
---
+++ youtube.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.commentThreads.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/code-samples#python
+
+import os
+import string
+import googleapiclient.discovery
+import json
+
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = "AIzaSyAwfyIG_RDJymt6j7Ic9slOvOjmZtlwc0k"
+
+    youtube: googleapiclient.discovery.Resource = googleapiclient.discovery.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+    request = youtube.commentThreads().list(
+        part="snippet,replies",
+        videoId="_VB39Jo8mAQ",
+        maxResults=2000
+    )
+    response = request.execute()
+
+    x = json.dumps(response, sort_keys=True, indent=4)
+    f = open('response.json', 'w')
+    f.write(x)
+
+
+if __name__ == "__main__":
+    main()
