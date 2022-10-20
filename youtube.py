89b4e528e34ec3f510fc423a07cc92a53fc45ecb
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-10-20 20:50:59 +0530

    refact: youtube.py

--- youtube.py
+++ youtube.py
@@ -1,70 +1,69 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.commentThreads.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
-
-from asyncio.windows_events import NULL
-from itertools import count
 import os
-from pydoc import doc
-import string
 import googleapiclient.discovery
 import json
-
 from utils.comment import process_comments
 
+API_NAME = "youtube"
+API_VER = "v3"
+with open('.env', 'r') as f:
+    KEY = f.read()
+    os.environ["KEY"] = KEY
 
-def getComments(channelId):
-    # # Disable OAuthlib's HTTPS verification when running locally.
-    # # *DO NOT* leave this option enabled in production.
-    # os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+def importComments():
+    """
+    Imports the comments from response.json and returns it as list
+    """
+    with open('response.json', 'r') as json_file:
+        user_data: list = json.loads(json_file.read())
+        return user_data
+
+
+def getComments(youtubeVideoId: str, MAX_COMMENT=2000):
+    """
+    Returns a list of 2000 comments. Saves the comments in response.json
+    """
     comment_list = []
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = "AIzaSyAwfyIG_RDJymt6j7Ic9slOvOjmZtlwc0k"
-    MAX_COMMENT = 2000
 
-    youtube: googleapiclient.discovery.Resource = googleapiclient.discovery.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
+    youtube = googleapiclient.discovery.build(API_NAME, API_VER, developerKey=os.environ["KEY"])
 
     request = youtube.commentThreads().list(
         part="snippet",
-        videoId=channelId,
+        videoId=youtubeVideoId,
         pageToken="",
         textFormat='plainText',
         order='relevance',
-        maxResults=100,  # 100 Is MAX limit possible, default is 20
+        maxResults=100,
     )
     response = request.execute()
 
     comment_list.extend(process_comments(response['items']))
 
-    while (response.get('nextPageToken', NULL) != NULL and len(comment_list) < MAX_COMMENT):
+    while (response.get('nextPageToken', None) != None and len(comment_list) < MAX_COMMENT):
         comment_list.extend(process_comments(response['items']))
         request = youtube.commentThreads().list(
             part="snippet",
-            videoId=channelId,
+            videoId=youtubeVideoId,
             pageToken=response['nextPageToken'],
             textFormat='plainText',
             order='relevance',
             maxResults=100,  # 100 Is MAX limit possible, default is 20
         )
         response = request.execute()
-        print(response)
+        print(f'Extracted : {len(comment_list)} Comments')
 
-    x = json.dumps(comment_list, indent=4)
+    data = json.dumps(comment_list, indent=4)
     f = open('response.json', 'w')
-    f.write(x)
-    
-    with open('response.json','r') as json_file:
-        user_data=json.loads(json_file.read())
-        for u in user_data:
-            print(u)
+    f.write(data)
+
+    return comment_list
 
 
 def main():
-    getComments('SffDXh8WLZo')
+    # Only use getComments() once. Then use importComments() for testing
+    # getComments('Ntn1-SocNiY')
+    comments = importComments()
+    print(comments[0])
 
 
 if __name__ == "__main__":
